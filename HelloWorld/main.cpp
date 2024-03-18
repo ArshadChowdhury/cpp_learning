1259b8fc0adba1d6853fb4b8d0a5f52f304e9408
Mohammed Arshad <arshadchowdhury46@gmail.com>
2024-03-19 02:22:17 +0600

    Learned pointer and functions of cpp in details

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -2,7 +2,54 @@
 
 #include <iostream>
 
-int main(){
-    std::cout << "Hello Project 1" << std::endl;
+using namespace std;
+
+int factorial (int n)
+{
+
+    int ans = 1;
+
+    for(int i=1; i<= n; i++)
+    {
+        ans = ans * i;
+    }
+    return ans;
+
+}
+
+int NCR ( int n, int r)
+{
+
+    int ans = factorial(n) / (factorial (n -r ) * factorial(r));
+    return ans;
+
+}
+
+int increment(int b)
+{
+    b = b + 1;
+    cout << b << endl;
+    return b;
+}
+
+int incrementByPtr ( int *cPtr)
+{
+    *cPtr = *cPtr + 1;
+    return *cPtr;
+}
+
+int main()
+{
+    cout << "Hello Project 1" << endl;
+    cout << factorial(5) << endl;
+    cout << NCR(5, 2) << endl;
+
+    int num = 10;
+
+    cout << num << endl;
+
+    cout << incrementByPtr(&num) << endl;
+    cout << incrementByPtr(&num) << endl;
+    cout << incrementByPtr(&num) << endl;
     return 0;
 }
